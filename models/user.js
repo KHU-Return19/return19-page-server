7a5ecaeca37cfc48602f3ec904acd00e9b61426d
hyun <seok02h@khu.ac.kr>
2021-08-19 14:31:48 +0900

    add user model

--- models/user.js
+++ models/user.js
@@ -3,5 +3,36 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     email:{
         type : String,
+        unique: true,
+        reqiued: true
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    name:{
+        type: String,
+        required:true
+    },
+    birthday:{
+        type:Date,
+        required: true,
+    },
+    interest:{
+        type: String,
+    },
+    bio :{
+        type: String,
+    },
+    img:{
+        type:String,
+    },
+    url : {
+        type:String
     }
+
+
+
 })
+const User = mongoose.model('User', userSchema)
+module.exports = { User }
