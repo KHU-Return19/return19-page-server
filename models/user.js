421535f7429d571e01a110113a1ddb938e341b16
hyun <seok02h@khu.ac.kr>
2021-08-19 17:01:33 +0900

    feat: add login

--- models/user.js
+++ models/user.js
@@ -41,10 +41,10 @@ const userSchema = new mongoose.Schema({
     }
 })
 
-userSchema.pre("save",function(next){
+userSchema.pre("save", function( next ){
     var user = this
     if(user.isModified("password")){
-        bcrypt.genSalt(saltRounds, function(err, salt){
+        bcrypt.genSalt(saltRounds, (err, salt) =>{
             if(err) return next(err)
             bcrypt.hash(user.password, salt, function(err, hash){
                 if(err) return next(err)
@@ -52,7 +52,37 @@ userSchema.pre("save",function(next){
                 next();
             })
         })
+    }else{
+        next()
     }
 })
+
+userSchema.methods.comparePassword = function(plainPassword, cb){
+    bcrypt.compare(plainPassword, this.password, (err, isMatch) => {
+        console.log(err)
+        if(err) return cb(err)
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb){
+    var user = this
+    var token = jwt.sign(user._id.toHexString(), "secretToken")
+    user.token = token
+    user.save((err,user)=>{
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
+
+userSchema.statics.findByToken = function(token, cb){
+    var user = this
+    jwt.verify(token, "secretToken", (err, decoded)=>{
+        user.findOne({"_id":decoded, "token":token}, (err,user)=>{
+            if(err) return cb(err)
+            cb(null, user)
+        })
+    })
+}
 const User = mongoose.model('User', userSchema)
 module.exports = { User }
--- routes/users.js
+++ routes/users.js
@@ -9,13 +9,16 @@ router.get('/',(req,res)=>{
 })
 
 router.post("/signup",(req,res)=>{
-    User.findOne({ email:req.body.email}, function(err,user){
-        if(err) return res.json({find_success:false, err})
+    User.findOne({ email:req.body.email}, (err,user) =>{
+        if(err) {
+            return res.json({find_success:false, err})
+        }
         if(user){
             return res.json({signup_success:false, msg:"이미 가입된 이메일 입니다."})
         }else{
             const newUser = new User(req.body)
             newUser.save((err, userInfo)=>{
+                console.log(err)
                 if(err) return res.json({ signup_success:false, err})
                 return res.status(201).json({
                     success: true,
@@ -25,4 +28,33 @@ router.post("/signup",(req,res)=>{
         }
     })
 })
+
+router.post("/login", (req,res)=> {
+    User.findOne({ email:req.body.email},(err, user)=>{
+        if(!user){
+            return res.json({
+                login_success:false,
+                msg: "존재하지 않는 이메일 입니다."
+            })
+        }
+        user.comparePassword(req.body.password, (err, isMatch) =>{
+            if(!isMatch){
+                return res.json({login_success: false,
+                    msg: "비밀번호가 틀렸습니다."
+                })
+            }else{
+                user.generateToken((err,user)=>{
+                    if(err) return res.send(err)
+                    res.cookie("x_auth", user.token).status(200).json({
+                        login_success: true,
+                        userId: user._id
+                    })
+                })
+            }
+        
+        })
+    })
+})
+
+
 module.exports = router
