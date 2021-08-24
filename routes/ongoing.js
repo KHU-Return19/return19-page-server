fb02d057e1ea56c6deb3718a6d98a9dc30461b2a
ejPark <qdw0313@khu.ac.kr>
2021-08-24 17:47:02 +0900

    feat: add ongoing page

--- routes/ongoing.js
+++ routes/ongoing.js
@@ -1,11 +1,45 @@
 const express = require('express')
 const router = express.Router()
 const { User } = require("../models/user")
-const { Board } = require("../models/study")
+const { Study } = require("../models/study")
 
-router.get('/ongoing', (req, res, next) => {
-    Study.find({}, function(err, study){
-        let list = Board.find()
-        return res.status(200).json({ data: list })
+router.get('/', (req, res, next) => {
+    Study.find(function(err, study){
+        if (err){
+            console.log(err)
+            return res.status(500).json({ message: "NaN" })
+        }
+        else{
+            console.log(study)
+            return res.status(200).json(study)
+        }
     })
 })
+
+router.get('/add', async(req, res) => {
+    // let new_study = new Study({
+    //     title: req.body.title,
+    //     content: req.body.info,
+    //     address: req.body.address,
+    //     isFin: req.body.isFin
+    // })
+
+    //test code
+    let new_study = new Study({
+        title: "test",
+        address: "http://google.com",
+        info: "test data",
+        isFin:false
+    })
+
+    new_study.save(function(err, data){
+        if (err){
+            console.log(err)
+        }
+        else {
+            console.log("saved")
+        }
+    })
+})
+
+module.exports = router
