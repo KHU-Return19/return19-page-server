34da071e635b9f2ed86b89c2cd5cbefe6cbdbed1
ejPark <qdw0313@khu.ac.kr>
2021-08-30 10:27:07 +0900

    Update auth.js from signup

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,18 @@
 const { User } = require("../models/user");
 
 let auth = (req, res, next) =>{
-    let token = req.cookies.x_auth;
-
-    User.findByToken(token, (err, user)=>{
-        if(err) throw err;
-        if(!user) return res.json({ isAuth:false, error:true})
-        req.token = token;
-        req.user = user;
-        next();
-    })
+    const token = req.header("x-auth-token")
+    if(!token) return res.status(400).json({get_token_success:false,
+    msg:"No token, authorization denied"})
+    else{
+        User.findByToken(token, (err, user)=>{
+            if(err) throw err;
+            if(!user) return res.json({ auth_success:false})
+            req.token = token;
+            req.user = user;
+            next()
+        })
+    }
 }
 
 module.exports = { auth };
